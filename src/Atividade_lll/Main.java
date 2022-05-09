d992b872f6d85bd785d2b44d6dd6703268c0c2eb
Adriel Silveira <99222181+AdrielSilveira@users.noreply.github.com>
2022-05-09 16:33:45 -0300

    Inicio da Atv.lll

---
+++ src/Atividade_lll/Main.java
@@ -0,0 +1,17 @@
+package Atividade_lll;
+
+
+public class Main{
+	
+	public static void main(String[] args) {
+		Ivetor vetAluno = new Ivetor(10);
+		
+		vetAluno.adiciona(new Aluno("Iago", 21, 10.0));
+		vetAluno.adiciona(new Aluno("Shiriu", 30, 6.0));
+		vetAluno.adiciona(new Aluno("DLM", 14, 3.1));
+
+		Aluno alunoTest = new Aluno("DLM", 14, 3.1);
+		System.out.println(vetAluno.contem(alunoTest));
+		System.out.println(vetAluno.tamanho());
+	}
+}
